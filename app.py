d6504a0ef22c10d6633eae7841a71fc8367c1002
TukeCS <tuke.richards@gmail.com>
2024-11-08 10:39:27 +0100

    Made Docker work properly with flask making the host 0.0.0.0

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
